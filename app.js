56439d2f1e4ac051b6b083baddf179c5171fa5c8
YOUR NAME <kenseioh@uw.edu>
2015-11-08 21:53:13 -0800

    l

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+var yoData;
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var secData;
+//var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var playlistUrl = 'https://api.spotify.com/v1/search?type=playlist&query='
 var myApp = angular.module('myApp', [])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
-  $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
-    })
+  $scope.getPlaylist = function(){
+	$http.get(playlistUrl + $scope.playlist).success(function(responses){
+		secData = $scope.playlists = responses.playlists.items
+	})
   }
+  
+  $scope.songlist = function(){
+	yoData = $scope.songlists.playlists.items
+  }
+  
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
